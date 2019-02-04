3e0662fb264d194a4d3d2690ea5c1c27c406f2c1
borisivnc <32333852+borisivnc@users.noreply.github.com>
2019-02-04 10:38:41 +0100

    Add files via upload

---
+++ NegativeOperatorException.java
@@ -0,0 +1,11 @@
+
+@SuppressWarnings("serial")
+public class NegativeOperatorException extends Exception {
+	
+	public void printStackTrace() {
+		
+		System.out.println("Operande negatif");
+		
+	}
+
+}
